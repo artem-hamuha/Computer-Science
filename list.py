e38a196616ed20e4245c075b1b2f8601ef0da4a8
Artem Hamuha <59758007+artem-hamuha@users.noreply.github.com>
2021-07-23 09:14:56 -0700

    combining list

--- list.py
+++ list.py
@@ -2,3 +2,9 @@ aList = [100, 200, 300, 400, 500]
 aList = aList[::-1]
 
 print(aList)
+
+list1 = ["M", "na", "i", "Ar"] 
+list2 = ["y", "me", "s", "tem"]
+list3 = [i + j for i, j in zip(list1, list2)]
+
+print(*list3, sep=" ")
