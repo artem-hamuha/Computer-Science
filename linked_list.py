6b029bc56f188fbaa67f8ee8b4c081236e928cec
Artem Hamuha <59758007+artem-hamuha@users.noreply.github.com>
2021-08-02 11:47:58 -0700

    last commit was dif code this one is linked list

--- linked_list.py
+++ linked_list.py
@@ -16,10 +16,15 @@ class Node(object):
 	def set_data (self, d):
 		self.data = d
 		
-	def __str__(self):
+	def has_next (self):
+		if self.get_next() is None:
+			return False
+		return True
+		
+	def to_string (self):
 		return "Node value: " + str(self.data)
-
-class CircularLinkedList (object):
+		
+class LinkedList (object):
 
 	def __init__ (self, r = None):
 		self.root = r
@@ -29,76 +34,83 @@ class CircularLinkedList (object):
 		return self.size
 
 	def add (self, d):
-		if self.get_size() == 0:
-			self.root = Node(d)
-			self.root.set_next(self.root)
-		else:
-			new_node = Node (d, self.root.get_next())
-			self.root.set_next(new_node)
+		new_node = Node (d, self.root)
+		self.root = new_node
 		self.size += 1
 
+	def add_node (self, n):
+		n.set_next(self.root)
+		self.root = n
+		self.size += 1
+		
 	def remove (self, d):
 		this_node = self.root
 		prev_node = None
 
-		while True:
-			if this_node.get_data() == d:	# found
+		while this_node is not None:
+			if this_node.get_data() == d:
 				if prev_node is not None:
 					prev_node.set_next(this_node.get_next())
 				else:
-					while this_node.get_next() != self.root:
-						this_node = this_node.get_next()
-					this_node.set_next(self.root.get_next())
-					self.root = self.root.get_next()
+					self.root = this_node.get_next()
 				self.size -= 1
 				return True     # data removed
-			elif this_node.get_next() == self.root:
-				return False	# data not found
-			prev_node = this_node
-			this_node = this_node.get_next()
+			else:
+				prev_node = this_node
+				this_node = this_node.get_next()
+		return False  # data not found
 
 	def find (self, d):
 		this_node = self.root
-		while True:
+		while this_node is not None:
 			if this_node.get_data() == d:
 				return d
-			elif this_node.get_next() == self.root:
+			elif this_node.get_next() == None:
 				return False
-			this_node = this_node.get_next()
-		
+			else:
+				this_node = this_node.get_next()
+				
 	def print_list (self):
 		print ("Print List..........")
 		if self.root is None:
 			return
 		this_node = self.root
-		print (this_node)
-		while this_node.get_next() != self.root:
+		print (this_node.to_string())
+		while this_node.has_next():
 			this_node = this_node.get_next()
-			print (this_node)
-
+			print (this_node.to_string())
+			
+	def sort (self):
+		if self.size > 1:
+			newlist = []
+			current = self.root
+			newlist.append(self.root)
+			while current.has_next():
+				current = current.get_next()
+				newlist.append(current)
+			newlist = sorted(newlist, key = lambda node: node.get_data(), reverse = True)
+			newll = LinkedList()
+			for node in newlist:
+				newll.add_node(node)
+			return newll
+		return self
+		
 def main():
-	myList = CircularLinkedList()
+	myList = LinkedList()
 	myList.add(5)
-	myList.add(7)
+	myList.add(9)
 	myList.add(3)
 	myList.add(8)
 	myList.add(9)
-	print("Find 8", myList.find(8))
-	print("Find 12", myList.find(12))
-
-	cur = myList.root
-	print (cur)
-	for i in range(8):
-		cur = cur.get_next()
-		print (cur)
-
 	print("size="+str(myList.get_size()))
+	myList.print_list()
+	myList = myList.sort()
 	myList.print_list()
 	myList.remove(8)
 	print("size="+str(myList.get_size()))
 	print("Remove 15", myList.remove(15))
 	print("size="+str(myList.get_size()))
-	myList.remove(5)	# delete root node
+	print("Find 25", myList.find(25))
 	myList.print_list()
 	
 main()
