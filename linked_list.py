b4fd72ea1d95f1ce4ebff4059e3c6c057491f0ef
Artem Hamuha <59758007+artem-hamuha@users.noreply.github.com>
2021-08-07 11:02:33 -0700

    linked list

--- linked_list.py
+++ linked_list.py
@@ -1,116 +1,49 @@
-class Node(object): #staged commit
+class Node:
+    def __init__ (self, data=None):
+        self.data = data
+        self.next = None
 
-	def __init__ (self, d, n = None):
-		self.data = d
-		self.next_node = n
+class linked_list:
+    def __init__(self):
+        self.head = Node()
+    
+    def append(self, data):
+        new_node = Node(data)
+        cur = self.head
+        while cur.next != None:
+            cur = cur.next
+        cur.next = new_node
+    
+    def lenght(self):
+        cur = self.head
+        total = 0
+        while cur.next != None:
+            total += 1
+            cur = cur.next
+        return total
 
-	def get_next (self):
-		return self.next_node
+    def display(self):
+        elements =[]
+        cur_node = self.head
+        while cur_node.next != None:
+            cur_node = cur_node.next
+            elements.append(cur_node.data)
+        return elements
+    
+    def get(self, index):
+        if index >= self.lenght():
+            print("ERROR: 'Get' Index out of range!")
+            return None
+        cur_index = 0
+        cur_node = self.head
+        while True:
+            cur_node = cur_node.next
+            if cur_index==index: return cur_node.data 
+            cur_index+=1
 
-	def set_next (self, n):
-		self.next_node = n
-		
-	def get_data (self):
-		return self.data
+my_list = linked_list()
 
-	def set_data (self, d):
-		self.data = d
-		
-	def has_next (self):
-		if self.get_next() is None:
-			return False
-		return True
-		
-	def to_string (self):
-		return "Node value: " + str(self.data)
-		
-class LinkedList (object):
+my_list.append(1)
+my_list.append(2)
 
-	def __init__ (self, r = None):
-		self.root = r
-		self.size = 0
-
-	def get_size (self):
-		return self.size
-
-	def add (self, d):
-		new_node = Node (d, self.root)
-		self.root = new_node
-		self.size += 1
-
-	def add_node (self, n):
-		n.set_next(self.root)
-		self.root = n
-		self.size += 1
-		
-	def remove (self, d):
-		this_node = self.root
-		prev_node = None
-
-		while this_node is not None:
-			if this_node.get_data() == d:
-				if prev_node is not None:
-					prev_node.set_next(this_node.get_next())
-				else:
-					self.root = this_node.get_next()
-				self.size -= 1
-				return True     # data removed
-			else:
-				prev_node = this_node
-				this_node = this_node.get_next()
-		return False  # data not found
-
-	def find (self, d):
-		this_node = self.root
-		while this_node is not None:
-			if this_node.get_data() == d:
-				return d
-			elif this_node.get_next() == None:
-				return False
-			else:
-				this_node = this_node.get_next()
-				
-	def print_list (self):
-		print ("Print List..........")
-		if self.root is None:
-			return
-		this_node = self.root
-		print (this_node.to_string())
-		while this_node.has_next():
-			this_node = this_node.get_next()
-			print (this_node.to_string())
-			
-	def sort (self):
-		if self.size > 1:
-			newlist = []
-			current = self.root
-			newlist.append(self.root)
-			while current.has_next():
-				current = current.get_next()
-				newlist.append(current)
-			newlist = sorted(newlist, key = lambda node: node.get_data(), reverse = True)
-			newll = LinkedList()
-			for node in newlist:
-				newll.add_node(node)
-			return newll
-		return self
-		
-def main():
-	myList = LinkedList()
-	myList.add(5)
-	myList.add(9)
-	myList.add(3)
-	myList.add(8)
-	myList.add(9)
-	print("size="+str(myList.get_size()))
-	myList.print_list()
-	myList = myList.sort()
-	myList.print_list()
-	myList.remove(8)
-	print("size="+str(myList.get_size()))
-	print("Remove 15", myList.remove(15))
-	print("size="+str(myList.get_size()))
-	print("Find 25", myList.find(25))
-	myList.print_list()
-	
-main()
+print(my_list.display())
