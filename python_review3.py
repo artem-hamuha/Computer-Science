b3d263104fa6a70b7809072e7a08ece40eac3876
Artem Hamuha <59758007+artem-hamuha@users.noreply.github.com>
2021-09-13 21:18:09 -0700

    boolean review

---
+++ python_review3.py
@@ -0,0 +1,16 @@
+good = True
+if good:
+    print('Good')
+
+#booleans are tru and false statments
+
+good = False
+if good:
+    print('Good')
+else:
+    print('Bad')
+    
+print(2>1)
+print(1>2)
+# booleans can also be returned usong boolean operators
+
