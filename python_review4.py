6ee386fe0bf439b4c5f12ab302cf71248b20283d
Artem Hamuha <59758007+artem-hamuha@users.noreply.github.com>
2021-09-15 18:30:03 -0700

    python review while and for loops

---
+++ python_review4.py
@@ -0,0 +1,26 @@
+for letters in 'Word':
+    print(letters)
+    
+#for loop
+#it iterates over the individual elements of the object you feed it
+
+list = [1, 'Cat', 2, 'Dog']
+for items in list:
+    print(items)
+    
+#for loops with lists
+
+list = ['Artem', [1, 'Cat'], [2, 'Dog']]
+print(list[0])
+print(list[1])
+print(list[2][1])
+
+#nested lists and printing elements of lists
+
+num = 0
+while num <= 4:
+    print(num)
+    num += 1
+    
+#while loop
+#basiclly while what you type is true keep repeating the stuff below
