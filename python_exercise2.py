5bbc6a53578f4fa4ababfa42d8a50644ea478587
Artem Hamuha <59758007+artem-hamuha@users.noreply.github.com>
2021-10-06 16:03:43 -0700

    fixed

--- python_exercise2.py
+++ python_exercise2.py
@@ -1,7 +1,9 @@
 #Two Sum
 class Solution:
-    def twoSum(self, nums: list[int], target: int) -> list[int]:
+    def twoSum(nums: list[int], target: int) -> list[int]:
         for i in range(len(nums)):
             for j in range(i + 1, len(nums)):
                 if nums[j] == target - nums[i]:
                     return [i, j]
+
+print(Solution.twoSum([-1,2,3,4,9], 7))
