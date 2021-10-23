86aed7ef304cf71c81fb7a2a4f5f1fabf80a548c
Artem Hamuha <59758007+artem-hamuha@users.noreply.github.com>
2021-10-22 19:05:27 -0700

    rotating array

---
+++ array_solution3.py
@@ -0,0 +1,19 @@
+class Solution:
+    def rotate(nums: list[int], k: int):
+        n = len(nums)
+        k %= n
+        
+        start = count = 0
+        while count < n:
+            current, prev = start, nums[start]
+            while True:
+                next_idx = (current + k) % n
+                nums[next_idx], prev = prev, nums[next_idx]
+                current = next_idx
+                count += 1
+                
+                if start == current:
+                    break
+            start += 1\
+
+print(Solution.rotate([1,2,3,4,5,6], 3))
