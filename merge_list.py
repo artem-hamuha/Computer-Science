e6e042f4c7a4fd6eabc86cf3af8ec9aa35d22e31
Artem Hamuha <59758007+artem-hamuha@users.noreply.github.com>
2021-11-11 12:34:22 -0800

    merging 2 lists(sorted)

--- merge_list.py
+++ merge_list.py
@@ -1,6 +1,12 @@
 class Solution:
-    def mergeLists(l1: list[int], l2: list[int]):
-        l3 = l1 + l2
-        return sorted(l3)
+    def mergeLists(l1, l2):
+        if l1 is None:
+            return l2
+        elif l2 is None:
+            return l1
+        else:
+            l3 = l1 + l2
+            return sorted(l3)
 
 print(Solution.mergeLists([1,2,4], [1,3,4]))
+print(Solution.mergeLists([],[]))
