39132bcdb0c21d5d1e0227bbcc51b7cefdf01c6b
Artem Hamuha <59758007+artem-hamuha@users.noreply.github.com>
2021-11-27 18:29:04 -0800

    Day 1 exercises

---
+++ Complete_Python_Pro_Bootcamp/Day1_exercise3.py
@@ -0,0 +1,4 @@
+#Write your code below this line 👇
+name = input("What is your name? ")
+#name = name.replace(" ", "") choice
+print(len(name))
---
+++ Complete_Python_Pro_Bootcamp/Day1_exercise4.py
@@ -0,0 +1,18 @@
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+a2 = a
+b2 = b
+a = b2
+b = a2
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
