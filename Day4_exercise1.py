071b0723b9e27bb578e9c1fcf366e3f69b1b86de
Artem Hamuha <59758007+artem-hamuha@users.noreply.github.com>
2021-12-01 17:39:29 -0800

    Day4 exercise

--- Day4_exercise1.py
+++ Day4_exercise1.py
@@ -1,2 +1,13 @@
 import random
-print(random.seed(123))
+# 🚨 Don't change the code below 👇
+test_seed = int(input("Create a seed number: "))
+random.seed(test_seed)
+ # 🚨 Don't change the code above 👆 It's only for testing your code.
+	 
+#Write your code below this line 
+num = random.randint(0,1)
+
+if num == 1:
+    print("Heads")
+elif num == 0:
+    print("Tails")
