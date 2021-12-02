50018872ee285f36f5e273a84456fa0a807282a0
Artem Hamuha <59758007+artem-hamuha@users.noreply.github.com>
2021-12-01 17:13:06 -0800

    Fixed day 3 project

--- Complete_Python_Pro_Bootcamp/Day3_Project.py
+++ Complete_Python_Pro_Bootcamp/Day3_Project.py
@@ -26,30 +26,30 @@ print("Your decisins will shape the story, survive and find the treasure or die.
 direction = input("You open your eyes and find yourself on an island where do you go. Left, right, or straight are your choices: ").lower()
 
 if direction == "left":
-    choice = input("""You end up finding a little hut with 3 doors\nthe red door\nthe blue door\nthe green door. Choices are:\nthe red door\nthe blue door\nthe green door - """).lower()
+    choice = input("""You end up finding a little hut with 3 doors\nthe red door\nthe blue door\nthe green door.\nWhich door would you like to go in - """).lower()
     if choice == "the read door":
         print("You got killed by a withch.")
-        print("Game Over: You Lose.\n Thank You For Playing!\nGoodbye.")
-    if choice == "the blue door":
+        print("Game Over: You Lose.\nThank You For Playing!\nGoodbye.")
+    elif choice == "the blue door":
         print("You walked in but, suddenly fell into a pit. You tryed to get out but, there was no way. You died of thirst.")
-        print("Game Over: You Lose.\n Thank You For Playing!\nGoodbye.")
-    if choice == "the green door":
-        print("You walked in and found a treasure chest.")
-        print("Game Over: You Win.\n Thank You For Playing!\nGoodbye.")
-if direction == "right":
+        print("Game Over: You Lose.\nThank You For Playing!\nGoodbye.")
+    elif choice == "the green door":
+        print("You walked in and found the treasure chest.")
+        print("Game Over: You Win.\nThank You For Playing!\nGoodbye.")
+elif direction == "right":
     print("You wanderd for 3 days and died of thirst.")
-    print("Game Over: You Lose.\n Thank You For Playing!\nGoodbye")
-if direction == "straight":
-    print("You enter the jungle. A Grizly Bear attacks you; running is not a choice because its to fast. What will you do: stick, fighting skills, play dead - ").lower()
+    print("Game Over: You Lose.\nThank You For Playing!\nGoodbye")
+elif direction == "straight":
+    choice = input("You enter the jungle. A Grizly Bear attacks you; running is not a choice because its to fast. What will you do: stick, fighting skills, play dead - ").lower()
     if choice == "stick":
         print("You died. No stick can over power a bear.")
-        print("Game Over: You Lose.\n Thank You For Playing!\nGoodbye")
-    if choice == "fighting skills":
+        print("Game Over: You Lose.\nThank You For Playing!\nGoodbye")
+    elif choice == "fighting skills":
         print("You died. Your fighting skills are impressive but, they cant overpower a bear.")
-        print("Game Over: You Lose.\n Thank You For Playing!\nGoodbye")
-    if choice == "play dead":
+        print("Game Over: You Lose.\nThank You For Playing!\nGoodbye")
+    elif choice == "play dead":
         print("You died. The bear was really hungry and ate you.")
-        print("Game Over: You Lose.\n Thank You For Playing!\nGoodbye")
+        print("Game Over: You Lose.\nThank You For Playing!\nGoodbye")
 else:
     print("That is not one of the choices. Play by the rules.\n Game Over: You Lose.")
     
