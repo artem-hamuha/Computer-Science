373e45061e928935b530e73ee83f0f2a256213df
Artem Hamuha <59758007+artem-hamuha@users.noreply.github.com>
2021-12-05 10:07:25 -0800

    Day6 exercises

---
+++ Complete_Python_Pro_Bootcamp/Day6_exercise1.py
@@ -0,0 +1,24 @@
+#1 solution for:
+#https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
+#https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%202&url=worlds%2Ftutorial_en%2Fhurdle2.json
+#https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%203&url=worlds%2Ftutorial_en%2Fhurdle3.json
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def jump():
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+        
+while not at_goal():
+    if wall_in_front():
+        jump()
+    else:
+        move()
