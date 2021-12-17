325020a6364f3e543ca1b57e6b4e2992ea0dabb5
Artem Hamuha <59758007+artem-hamuha@users.noreply.github.com>
2021-12-16 16:34:39 -0800

    1st part done

---
+++ Complete_Python_Pro_Bootcamp/Day7_part1.py
@@ -0,0 +1,12 @@
+import random
+#Things to do to make hangman game
+
+#auto pick random word
+word_list = ["Richard", "Apple", "Misconseption"]
+x = random.randint(0, 2)
+chosen_word = word_list[x]
+
+print(chosen_word)
+
+
+
