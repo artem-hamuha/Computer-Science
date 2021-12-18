a9261395ad1c29dfe8b2511ce4e01c3159bff755
Artem Hamuha <59758007+artem-hamuha@users.noreply.github.com>
2021-12-17 16:45:24 -0800

    more parts to the project

--- Complete_Python_Pro_Bootcamp/Day7_part1.py
+++ Complete_Python_Pro_Bootcamp/Day7_part1.py
@@ -2,11 +2,20 @@ import random
 #Things to do to make hangman game
 
 #auto pick random word
-word_list = ["Richard", "Apple", "Misconseption"]
-x = random.randint(0, 2)
-chosen_word = word_list[x]
+word_list = ["richard", "apple", "misconception"]
 
-print(chosen_word)
+chosen_word = random.choice(word_list)
+print(f"Hint {chosen_word} is trhe chosen word")
+#ask for letter input
+letter = input("Enter letter - ").lower()
 
+#check if letter in word
 
-
+if letter == chosen_word:
+    print(f"it is {chosen_word} you win")
+else:
+    for l in chosen_word:
+        if l == letter:
+            print(l)
+        else:
+            print("-")
