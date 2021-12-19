71a9086322b85073c61eecb794511d706408158d
Artem Hamuha <59758007+artem-hamuha@users.noreply.github.com>
2021-12-18 22:25:34 -0800

    unfinished check of letter

--- Complete_Python_Pro_Bootcamp/Day7_part1.py
+++ Complete_Python_Pro_Bootcamp/Day7_part1.py
@@ -3,19 +3,96 @@ import random
 
 #auto pick random word
 word_list = ["richard", "apple", "misconception"]
-
 chosen_word = random.choice(word_list)
-print(f"Hint {chosen_word} is trhe chosen word")
+#adding lives and keeping track of them
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+lives = range(len(stages))
+
+#for testing purposes
+print(f"Hint, {chosen_word} is the chosen word")
+
 #ask for letter input
 letter = input("Enter letter - ").lower()
 
-#check if letter in word
 
-if letter == chosen_word:
-    print(f"it is {chosen_word} you win")
-else:
-    for l in chosen_word:
-        if l == letter:
-            print(l)
-        else:
-            print("-")
+#check if letter in word and loop game
+display = []
+for _ in range(len(chosen_word)):
+    display += "_"
+
+game_over = False
+while game_over == False:
+    if lives == 0:
+        print(stages[lives])
+        print("Game Over, You Lose!")
+        game_over = True
+
+    elif letter == chosen_word:
+        print(f"It is {chosen_word}, you win!")
+        
+    else:
+        count = 0
+        for l in chosen_word:
+            if l == letter:
+                display[count] = letter
+                count += 1
+            else:
+                count += 1
+        
+
