9cf29773662081b5cf22f82c3e602c88ba5f4d38
Artem Hamuha <59758007+artem-hamuha@users.noreply.github.com>
2021-12-29 11:06:03 -0800

    updated algorithm

--- Dijkstras_algorithm.py
+++ Dijkstras_algorithm.py
@@ -1,3 +1,27 @@
+graph = {}
+graph["start"] = {}
+graph["start"]["a"] = 6
+graph["start"]["b"] = 2
+graph["a"] = {}
+graph["a"]["fin"] = 1
+graph["b"] = {}
+graph["b"]["a"] = 3
+graph["b"]["fin"] = 5
+graph["fin"] = {}
+
+infinity = float("inf")
+costs = {}
+costs["a"] = 6
+costs["b"] = 2
+costs["fin"] = infinity
+
+parents = {}
+parents["a"] = "start"
+parents["b"] = "start"
+parents["fin"] = None
+
+processed = []
+
 def find_lowest_cost_node(costs):
     lowest_cost = float("inf")
     lowest_cost_node = None
