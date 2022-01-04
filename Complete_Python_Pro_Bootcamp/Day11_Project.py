129ba2d5bdec513f1dd0c04e278cdfe24917d747
Artem Hamuha <59758007+artem-hamuha@users.noreply.github.com>
2022-01-03 20:52:46 -0800

    uncomplete version of simplified blackjack

--- Complete_Python_Pro_Bootcamp/Day11_Project.py
+++ Complete_Python_Pro_Bootcamp/Day11_Project.py
@@ -14,7 +14,7 @@ logo = """
 yes_no = ["yes", "no"]
 
 def deal_card(who: list):
-    """Puts card in designated list."""
+    """For dealing cards in the begining of the game."""
     cards = [11, 11, 11, 11, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10]
     card = random.choice(cards)
     who.append(card)
@@ -36,15 +36,81 @@ def check_ace(hand):
         count += 1
 
 
-dealer_hand = [11, 11]
 
-player_hand = [10, 5]
 
-print(dealer_hand)
-check_ace(dealer_hand)
-print(dealer_hand)
 
-#play = input("Would you like to play BlackJack?[yes/no]\n").lower()
-#if play not in yes_no:
-#    print("Invalid input, try again.")
-#    play = input("Would you like to play BlackJack?[yes/no]\n").lower()
+print(logo)
+
+play = input("Would you like to play BlackJack? [yes/no]\n").lower()
+while play not in yes_no:
+    print("Invalid input, try again.")
+    play = input("Would you like to play BlackJack? [yes/no]\n").lower()
+
+while play == "yes":
+    player_hand = []
+
+    dealer_hand = []
+
+    while len(player_hand) != 2 and len(dealer_hand) != 2:
+        deal_card(player_hand)
+        deal_card(dealer_hand)
+
+    dealer_hand_1c = ["?", dealer_hand[1]]
+    
+    print(f"\nYour Hand: {player_hand}\nTotal: {sum(player_hand)}")
+    print(f"\nDealer Hand: {dealer_hand_1c}\nTotal: {dealer_hand[1]}")
+
+    if sum(player_hand) == 21:
+        print("\nBlackJack!!!\nYou Win!")
+        break
+
+    hit_stand = input("Would you like to hit [yes/no]\n").lower()
+
+    while hit_stand not in yes_no:
+        print("\nInvalid input, try again.")
+        hit_stand = input("Would you like to hit [yes/no]\n").lower()
+    
+    while hit_stand == "yes":
+        check_ace(player_hand)
+        hit(player_hand)
+        check_ace(player_hand)
+
+        if sum(player_hand) > 21:
+            print(f"\nYour Hand: {player_hand}\nTotal: {sum(player_hand)}")
+            print("\nYou Bust.\nDealer Wins!")
+            break
+
+        print(f"\nYour Hand: {player_hand}\nTotal: {sum(player_hand)}")
+        hit_stand = input("\nWould you like to hit [yes/no]\n").lower()
+
+        while hit_stand not in yes_no:
+            print("\nInvalid input, try again.")
+            hit_stand = input("Would you like to hit [yes/no]\n").lower()
+    
+    print(f"Dealer Hand: {dealer_hand}\nTotal: {sum(dealer_hand)}")
+
+    while sum(dealer_hand) < 17:
+        check_ace(dealer_hand)
+        hit(dealer_hand)
+        check_ace(dealer_hand)
+
+        if sum(dealer_hand) > 21:
+            print(f"Dealer Hand: {dealer_hand}\nTotal: {sum(dealer_hand)}")
+            print("Dealer Busts.\nPlayer Wins!")
+            break
+
+        print(f"Dealer Hand: {dealer_hand}\nTotal: {sum(dealer_hand)}")
+    
+    if dealer_hand < player_hand:
+        print("Player Wins!")
+    
+    elif dealer_hand > player_hand:
+        print("Dealer Wins!")
+    
+    else:
+        print("Push.")
+    
+    play = input("\nWould you like to play again? [yes/no]\n").lower()
+    while play not in yes_no:
+        print("\nInvalid input, try again.")
+        play = input("Would you like to play BlackJack? [yes/no]\n").lower()
