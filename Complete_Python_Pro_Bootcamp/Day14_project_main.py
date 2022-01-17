08bbf9d33cf960f155c020f4b751a36efb32d61b
Artem Hamuha <59758007+artem-hamuha@users.noreply.github.com>
2022-01-17 14:06:16 -0800

    done game

--- Complete_Python_Pro_Bootcamp/Day14_project_main.py
+++ Complete_Python_Pro_Bootcamp/Day14_project_main.py
@@ -2,19 +2,58 @@ from Day14_project_data import data, logo, vs
 import random
 import os
 
-higher_lower = ["higher", "lower"]
+def get_random_account():
+  """Get data from random account"""
+  return random.choice(data)
 
-game_over = False
-while not game_over:
-    p1 = random.choice(range(0, len(data)))
-    p2 = random.choice(range(0, len(data)))
-    while p1 == p2:
-        p2 = random.choice(range(0, len(data)))
+def format_data(account):
+  """Format account into printable format: name, description and country"""
+  name = account["name"]
+  description = account["description"]
+  country = account["country"]
+  # print(f'{name}: {account["follower_count"]}')
+  return f"{name}, a {description}, from {country}"
 
-    p1_ = data[p1]
-    p2_ = data[p2]   
+def check_answer(guess, a_followers, b_followers):
+  """Checks followers against user's guess 
+  and returns True if they got it right.
+  Or False if they got it wrong.""" 
+  if a_followers > b_followers:
+    return guess == "a"
+  else:
+    return guess == "b"
 
-    print(logo)
-    print(f"Who has more foloowers on Instagram\n{p1_}\n{vs}\n{p2_}")
-    game_over = True
+
+def game():
+  print(logo)
+  score = 0
+  game_should_continue = True
+  account_a = get_random_account()
+  account_b = get_random_account()
+
+  while game_should_continue:
+    account_a = account_b
+    account_b = get_random_account()
+
+    while account_a == account_b:
+      account_b = get_random_account()
+
+    print(f"Compare A: {format_data(account_a)}.")
+    print(vs)
+    print(f"Against B: {format_data(account_b)}.")
     
+    guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+    a_follower_count = account_a["follower_count"]
+    b_follower_count = account_b["follower_count"]
+    is_correct = check_answer(guess, a_follower_count, b_follower_count)
+
+    os.system("CLS")
+    print(logo)
+    if is_correct:
+      score += 1
+      print(f"You're right! Current score: {score}.")
+    else:
+      game_should_continue = False
+      print(f"Sorry, that's wrong. Final score: {score}")
+
+game()
