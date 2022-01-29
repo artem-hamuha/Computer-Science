e0ca69e121f50acbb79652abf58fe6e37318e79b
Artem Hamuha <59758007+artem-hamuha@users.noreply.github.com>
2022-01-29 10:10:33 -0800

    psome practice with turtle module

---
+++ Complete_Python_Pro_Bootcamp/Day18_practice.py
@@ -0,0 +1,20 @@
+from turtle import Turtle, Screen
+
+my_turtle = Turtle()
+my_turtle.shape("triangle")
+
+my_turtle.color("cyan")
+my_turtle.pencolor("blue")
+
+my_turtle.fd(150)
+my_turtle.left(90)
+my_turtle.fd(150)
+my_turtle.left(90)
+my_turtle.fd(150)
+my_turtle.left(90)
+my_turtle.fd(150)
+
+
+
+screen = Screen()
+screen.exitonclick()
