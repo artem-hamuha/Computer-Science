b0b4029c85da7e59cc5eb06be01e836deed7ebaf
Artem Hamuha <59758007+artem-hamuha@users.noreply.github.com>
2022-01-31 07:10:15 -0800

    drawingn dashed line and square to decagon

--- Complete_Python_Pro_Bootcamp/Day18_practice.py
+++ Complete_Python_Pro_Bootcamp/Day18_practice.py
@@ -6,6 +6,7 @@ my_turtle.shape("triangle")
 my_turtle.color("cyan")
 my_turtle.pencolor("blue")
 
+#square
 my_turtle.fd(150)
 my_turtle.left(90)
 my_turtle.fd(150)
@@ -13,6 +14,26 @@ my_turtle.left(90)
 my_turtle.fd(150)
 my_turtle.left(90)
 my_turtle.fd(150)
+
+#dashed line
+for i in range(8):
+     my_turtle.fd(10)
+     my_turtle.pu()
+     my_turtle.fd(10)
+     my_turtle.pd()
+
+#square to decagon
+def draw_figure(turtle, sides):
+    angle = 360/ sides
+    for i in range(0, sides):
+        turtle.fd(100)
+        turtle.right(angle)
+
+i = 4
+while i <= 10:
+    draw_figure(my_turtle, i)
+    i += 1
+
 
 
 
