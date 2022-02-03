01756de7cf426f5a24acb61e7fa492dc338cf8a8
Artem Hamuha <59758007+artem-hamuha@users.noreply.github.com>
2022-02-03 05:57:36 -0800

    Day 18 hursed painting project done

--- Complete_Python_Pro_Bootcamp/Day18/Day18_Project.py
+++ Complete_Python_Pro_Bootcamp/Day18/Day18_Project.py
@@ -1,8 +1,29 @@
 #hursed painting
-from turtle import Turtle
-import colorgram
+from turtle import Screen, Turtle
+import turtle as t
+import random
 
-colors = colorgram.extract("image.jpg", 6)
-print(colors)
+colors = [[160, 160, 160], [255, 119, 60], [127, 0, 255], [255, 0, 255], [32, 32, 32]]
 
+t.colormode(255)
 pen = Turtle()
+pen.ht()
+pen.speed('fast')
+pen.penup()
+pen.goto(-125, -25)
+pen_location = [-125, -25]
+
+for i in range(0, 10):
+    pen_location[1] += 25
+    pen.goto(pen_location)
+    for i in range(0, 10):
+        r_color = random.choice(colors)
+        pen.color(r_color)
+        pen.dot(20)
+        pen.fd(25)
+
+
+
+
+screen = Screen()
+screen.exitonclick()
