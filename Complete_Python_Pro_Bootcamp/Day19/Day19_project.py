3830fb95c0390aec9c45ed560fed358be61eddc8
Artem Hamuha <59758007+artem-hamuha@users.noreply.github.com>
2022-02-07 21:57:11 -0800

    turtle race beting game

--- Complete_Python_Pro_Bootcamp/Day19/Day19_project.py
+++ Complete_Python_Pro_Bootcamp/Day19/Day19_project.py
@@ -1,42 +1,56 @@
-from turtle import Turtle, Screen, screensize
+from turtle import Turtle, Screen
+import random
 
-pen = Turtle()
 screen = Screen()
 screen.setup(500, 450)
 screen.bgcolor("black")
 
-t_colors = ["red", "yellow", "blue", "brown", "green"]
+t_colors = ["red", "yellow", "blue", "white", "green"]
+y_cords = [-60, -20, 20, 60, 100]
+all_turtles = []
 
-pen.speed("normal")
-pen.shape("turtle")
-pen.up()
-pen.color("red")
+for index in range(5):
+    pen = Turtle()
+    pen.speed("slowest")
+    pen.shape("turtle")
+    pen.up()
+    pen.color(t_colors[index])
+    pen.goto(-225, y_cords[index])
+    all_turtles.append(pen)
 
-pen2 = pen.clone()
-pen2.color("blue")
+ways_to_move = random.randint(1, 10)
 
-pen3 = pen.clone()
-pen3.color("yellow")
+user_bet = int(screen.textinput("Make your bet.", "Bet: "))
 
-pen4 = pen.clone()
-pen4.color("brown")
+while not user_bet:
+    user_bet = int(screen.textinput("No bet entered", "Bet: "))
 
-pen5 = pen.clone()
-pen5.color("green")
-
-pen.goto(-225, -60)
-pen2.goto(-225, -20)
-pen3.goto(-225, 20)
-pen4.goto(-225, 60)
-pen5.goto(-225, 100)
-
-user_bet = screen.textinput("Make your bet.", "Amount of money: ")
-user_turtle = screen.textinput("Pick your turtle.", "Turtle color: ")
+user_turtle = screen.textinput("Pick your turtle.", "Turtle color: ").lower()
 
 while user_turtle not in t_colors:
-    user_turtle = screen.textinput("Turtle not found.", "Turtle color: ")
+    user_turtle = screen.textinput("Turtle not found.", "Turtle color: ").lower()
+
+race_on = True
+
+while race_on:
+    for turtle in all_turtles:
+
+        if turtle.xcor() > 240:
+
+            if user_turtle == turtle.pencolor():
+                print(f"{turtle.pencolor().title()} turtle wins!\nYou win {user_bet * 2}$!")
+                race_on = False
+            
+            else:
+                print(f"{turtle.pencolor().title()} turtle wins!\nYou lose {user_bet}$!")
+                race_on = False
+
+        r_distance = random.randint(1, 10)
+        turtle.forward(r_distance)
+    
 
 
+    
 
 screen.exitonclick()
 
