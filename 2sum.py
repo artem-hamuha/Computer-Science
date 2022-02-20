86e7265104c569e4f966b50395943aaa92ae6d64
Artem Hamuha <59758007+artem-hamuha@users.noreply.github.com>
2022-02-20 12:54:29 -0800

    2 sum one pass solution

--- 2sum.py
+++ 2sum.py
@@ -1,11 +1,15 @@
 nums = [0, 5, 6, 15, 3, 8]
 target = 18
 
-for i in range(len(nums)):
-    dl = nums
-    diff = target - nums[i]
-    dl[i]=None
-    if(diff in dl):
-        res = []
-        res.extend([i,dl.index(diff)])
-        print(res)
+def two_sum(list, target):
+    prevMap = {}
+
+    for i, n in enumerate(list):
+        diff = target - n
+        if diff in prevMap:
+            return prevMap[diff], i
+        prevMap[n] = i
+
+
+
+two_sum(nums, target)
