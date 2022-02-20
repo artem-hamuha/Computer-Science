c57c30d3b06f3d7f17d1071833da18a881f3c63d
Artem Hamuha <59758007+artem-hamuha@users.noreply.github.com>
2022-02-20 11:32:00 -0800

    solution 2 for valid angram

--- valid_anagram.py
+++ valid_anagram.py
@@ -1,17 +1,28 @@
 word1 = "interpersonal"
 word2 = "niretpernaso"
+import datetime
+start_time = datetime.datetime.now()
 
 def valid_anagram(word1, word2):
     if word1 == word2:
-        print(True)
+        return True
     elif len(word1) == len(word2):
         word1 = list(word1)
         word2 = list(word2)
         word1.sort()
         word2.sort()
         if word1 == word2:
-            print(True)
+            return True
     else:
-        print(False)
+        return False
 
 valid_anagram(word1, word2)
+
+end_time = datetime.datetime.now()
+print(end_time - start_time)
+
+start_time = datetime.datetime.now()
+
+#solution 2
+def valid_anagram(word1, word2):
+    return sorted(word1) == sorted(word2)
