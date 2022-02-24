3ed41e8196b27631686a0910b24394fb87809096
Artem Hamuha <59758007+artem-hamuha@users.noreply.github.com>
2022-02-24 09:15:54 -0800

    bank robbber own code

--- house_robber.py
+++ house_robber.py
@@ -1,14 +1,25 @@
-from operator import index
-
-
+#my code
 def house_robber(nums):
-    if len(nums)<=2: return max(nums)
-    robbing=[nums[0],max(nums[0],nums[1])]
-    for i in range(2,len(nums)):
-        robbing.append(max(nums[i]+robbing[-2],robbing[-1]))
-    return robbing[-1]
-        
+    max_num = max(nums)
+    if nums.index(max_num) == 0:
+        right = nums[nums.index(max_num) + 1]
+        nums.remove(right)
+        nums.remove(max_num)
+        max_num2 = max(nums)
+        return max_num + max_num2
+    elif nums.index(max_num) == len(nums) - 1:
+        left = nums[nums.index(max_num) - 1]
+        nums.remove(left)
+        nums.remove(max_num)
+        max_num2 = max(nums)
+        return max_num + max_num2
+    else:
+        left = nums[nums.index(max_num) - 1]
+        right = nums[nums.index(max_num) + 1]
+        nums.remove(left)
+        nums.remove(right)
+        nums.remove(max_num)
+        max_num2 = max(nums)
+        return max_num + max_num2
 
-    
-
-print(house_robber([1, 1, 3, 1]))
+print(house_robber([1, 2, 9, 4, 5, 0, 8]))
